a4907a2e0e831923785096873f8debd4fbc8446d
Anubhav Lohani <anubhavlohani4@gmail.com>
2023-01-07 23:47:35 +0530

    shifted password hashing to helpers

--- api/crud.py
+++ api/crud.py
@@ -1,20 +1,17 @@
 from sqlalchemy.orm import Session
-from werkzeug.security import generate_password_hash, check_password_hash
 
 from . import models, schemas
 
 
-def get_user(db: Session, username: int):
-    return db.query(models.User).filter(models.User.username == username).first()
+def get_user(db: Session, username: int) -> models.User:
+	return db.query(models.User).filter(models.User.username == username).first()
 
 def create_user(db: Session, user: schemas.UserSignUp) -> models.User:
-    existing_user = get_user(db, user.username)
-    if existing_user:
-        return existing_user
-    hashed_password = generate_password_hash(user.password, 'sha256')
-    user.password = hashed_password
-    db_user = models.User(**user.dict())
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return db_user
+	existing_user = get_user(db, user.username)
+	if existing_user:
+		return existing_user
+	db_user = models.User(**user.dict())
+	db.add(db_user)
+	db.commit()
+	db.refresh(db_user)
+	return db_user
