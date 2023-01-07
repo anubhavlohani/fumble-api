dd8151754de542836f20fe5ab2b41c304906ab5b
Anubhav Lohani <anubhavlohani4@gmail.com>
2023-01-07 19:55:00 +0530

    working signup

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 import uvicorn
 
@@ -8,6 +9,16 @@ from .database import SessionLocal, engine
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+origins = [
+    "http://localhost:3000"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Dependency
 def get_db():
@@ -21,13 +32,22 @@ def get_db():
 def home():
     return "Ahh, I see you've found this API 🦄. Welcome 🦚"
 
+@app.get("/get-user")
+def does_user_exist(username: str, db: Session = Depends(get_db)):
+    user = crud.get_user(db, username)
+    data = {
+        'found': True if user else False,
+        'user': user
+    }
+    return data
+
 @app.post("/signup")
 def sign_up(user: schemas.UserSignUp, db: Session = Depends(get_db)):
-    db_user = schemas.UserCreate(**user.dict())
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return db_user
+    try:
+        crud.create_user(db, user)
+    except Exception as err:
+        print(err)
+    return {'success': True}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
