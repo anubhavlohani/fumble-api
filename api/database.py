5dfd172fc73ac2e871a46a7355408af14a0c4329
Anubhav Lohani <anubhavlohani4@gmail.com>
2023-01-07 23:48:35 +0530

    now importing config

--- api/database.py
+++ api/database.py
@@ -3,13 +3,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+from .config import SQLALCHEMY_DATABASE_URL
 
 
-curr_dir = os.path.abspath(os.path.dirname(__name__))
-SQLALCHEMY_DATABASE_URL = "sqlite:///" + os.path.join(curr_dir, 'database.sqlite3')
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+	SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
