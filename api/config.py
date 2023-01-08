c2929ba5db2aabecc748a80e8b60d18db22af5e2
Anubhav Lohani <anubhavlohani4@gmail.com>
2023-01-08 20:37:06 +0530

    deleted config.py for github

--- api/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-curr_dir = os.path.abspath(os.path.dirname(__name__))
-SQLALCHEMY_DATABASE_URL = "sqlite:///" + os.path.join(curr_dir, 'database.sqlite3')
-
-SECRET_KEY = 'wellWellWellIfItAintTheInvisibleC*nt'
-ALGORITHM = "HS256"
