ce18f68931f2a08088c9af199ac568d9eef8243a
Anubhav Lohani <anubhavlohani4@gmail.com>
2023-01-09 13:42:01 +0530

    no relative imports

--- api/main.py
+++ api/main.py
@@ -5,8 +5,8 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 import uvicorn
 
-from . import models, schemas, crud, helpers
-from .database import SessionLocal, engine
+import models, schemas, crud, helpers
+from database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
 
