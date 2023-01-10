b896e5b9e9d17cf8c1c63cad445c01c8cf44be49
Anubhav Lohani <anubhavlohani4@gmail.com>
2023-01-10 19:46:48 +0530

    added deployed netlify website to cors

--- api/main.py
+++ api/main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 app.add_middleware(
 	CORSMiddleware,
-	allow_origins=["http://localhost:3000"],
+	allow_origins=["http://localhost:3000", "https://exquisite-salamander-ca0e85.netlify.app"],
 	allow_credentials=True,
 	allow_methods=["*"],
 	allow_headers=["*"],
