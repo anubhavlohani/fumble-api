a68c5d4e721fe18352b019148719083fc9a15f59
Anubhav Lohani <anubhavlohani4@gmail.com>
2023-01-14 18:02:26 +0530

    added func to return all stories

--- api/crud.py
+++ api/crud.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Session
+import tekore as tk
 
-from . import models, schemas
+from . import models, schemas, helpers
 
 
 def get_user(db: Session, username: int) -> models.User:
@@ -24,3 +25,17 @@ def create_story(db: Session, user: models.User, story: schemas.NewStory) -> mod
 	db.commit()
 	db.refresh(new_story)
 	return new_story
+
+def all_stories(db: Session, spotify: tk.Spotify) -> list[schemas.DetailedStory]:
+	stories = db.query(models.Story)
+	detailed_stories = []
+	for story in stories:
+		track = helpers.track_details(spotify, story.track_id)
+		detailed_story = schemas.DetailedStory(
+			username=story.user.username,
+			track=track,
+			caption=story.caption,
+			time_created=story.time_created
+		)
+		detailed_stories.append(detailed_story)
+	return detailed_stories
