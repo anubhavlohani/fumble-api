d7c2b62ac4124ce1cc44b5b7b7e029d131b5d2a7
Anubhav Lohani <anubhavlohani4@gmail.com>
2023-01-20 20:22:08 +0530

    added func to create new like record

--- api/crud.py
+++ api/crud.py
@@ -34,14 +34,25 @@ def all_stories(db: Session, spotify: tk.Spotify) -> list[schemas.DetailedStory]
 	detailed_stories = []
 	for story in stories:
 		track = helpers.track_details(spotify, story.track_id)
+		store_like_records = db.query(models.Likes).filter(models.Likes.story_id == story.id).all()
+		liked_by = [record.user_id for record in store_like_records]
 		detailed_story = schemas.DetailedStory(
 			id=story.id,
 			username=story.user.username,
 			track=track,
 			caption=story.caption,
-			time_created=story.time_created
+			time_created=story.time_created,
+			liked_by=liked_by
 		)
 		detailed_stories.append(detailed_story)
 	# sort by time by default
 	detailed_stories = sorted(detailed_stories, key=lambda x: x.time_created, reverse=True)
 	return detailed_stories
+
+def like_story(db: Session, like: schemas.LikeStory) -> models.Likes:
+	like_data = like.dict()
+	new_like = models.Likes(**like_data)
+	db.add(new_like)
+	db.commit()
+	db.refresh(new_like)
+	return new_like
